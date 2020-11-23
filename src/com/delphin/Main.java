fb835fa5daaf8a309e47710f6b3dcb5406a3cf46
Mick3460 <73465505+Mick3460@users.noreply.github.com>
2020-11-23 13:00:27 +0100

    Vi laver nu nye filer hver gang vi laver et medlem.

--- src/com/delphin/Main.java
+++ src/com/delphin/Main.java
@@ -1,5 +1,6 @@
 package com.delphin;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
@@ -7,15 +8,30 @@ public class Main {
     void run() throws IOException {
         IDNumber idNumber = new IDNumber();
 
+        Member test3 = new Member(idNumber.newMemberID(), 35,"Jesus",true,false,true,false,1500);
+        int idtallet = test3.getMemberID();
+        String idtal = Integer.toString(idtallet);
+        createNewMemberFile(idtal);
 
-        Member test = new Member(idNumber.newMemberID(),55, "Mick");
-        test.methodLol(test);
-        System.out.println(test.toString());
 
-        Member test2 = new Member(idNumber.newMemberID(),14,"yaayEet");
-        test2.methodLol(test2);
+
+        //System.out.println("YAYEET");
+        //System.out.println(test3.toString());
     }
 
+    void createNewMemberFile(String IDNumber){ // @author Stackoverflow :)
+        String path = "src\\com\\delphin\\"+IDNumber+".txt";
+        try{
+            File viProver = new File(path);
+            if (viProver.createNewFile()){
+                System.out.println("File created: " + viProver.getName());
+            }else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
--- src/com/delphin/Member.java
+++ src/com/delphin/Member.java
@@ -7,6 +7,12 @@ public class Member {
    private int age;
    private String name;
    private int memberID;
+   private boolean activity; //true = active member, not passive
+   private boolean junior; //true = Under the age of 18
+   private boolean competitor; //true = competes
+   private boolean activeDebt; //true = has arrears
+   private double subscriptionPrice; //use method to calculate
+
 
    static File membersData = new File("src\\com\\delphin\\Members.txt");
 
@@ -19,6 +25,52 @@ public class Member {
 
    }
 
+   Member(int memberID, int age, String name, boolean activity, boolean junior, boolean competitor, boolean activeDebt, double subscriptionPrice) throws IOException {
+       this.memberID=memberID;
+       this.age=age;
+       this.name=name;
+       this.activity=activity;
+       this.junior=junior;
+       this.competitor=competitor;
+       this.activeDebt=activeDebt;
+       this.subscriptionPrice=subscriptionPrice;
+   }
+
+   public boolean isActivity() {
+        return activity;
+    }
+   public void setActivity(boolean activity) {
+        this.activity = activity;
+    }
+
+   public boolean isJunior() {
+        return junior;
+    }
+   public void setJunior(boolean junior) {
+        this.junior = junior;
+    }
+
+   public boolean isCompetitor() {
+        return competitor;
+    }
+   public void setCompetitor(boolean competitor) {
+        this.competitor = competitor;
+    }
+
+   public boolean isActiveDebt() {
+        return activeDebt;
+    }
+   public void setActiveDebt(boolean activeDebt) {
+        this.activeDebt = activeDebt;
+    }
+
+   public double getSubscriptionPrice() {
+        return subscriptionPrice;
+    }
+   public void setSubscriptionPrice(double subscriptionPrice) {
+        this.subscriptionPrice = subscriptionPrice;
+    }
+
    public int getMemberID(){return this.memberID;}
    public void setMemberID(int memberID){this.memberID=memberID;}
 
@@ -28,15 +80,25 @@ public class Member {
    public String getName() {return this.name;}
    public void setName(String name) {this.name=name;}
 
+    @Override
+    public String toString() {
+        return "Member{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", memberID=" + memberID +
+                ", activity=" + activity +
+                ", junior=" + junior +
+                ", competitor=" + competitor +
+                ", activeDebt=" + activeDebt +
+                ", subscriptionPrice=" + subscriptionPrice +
+                '}';
+    }
+
    Scanner readFile = new Scanner(membersData);
    FileWriter fw = new FileWriter(membersData,true);
    PrintWriter writeToFile = new PrintWriter(fw);
 
-   public String toString() {
-       return getMemberID() + " " + getName() + " " + getAge();
-   }
-
-   void methodLol(Object member) throws IOException {
+    void methodLol(Object member) throws IOException {
        writeToFile.println(member.toString());
        writeToFile.flush();
        writeToFile.close();
