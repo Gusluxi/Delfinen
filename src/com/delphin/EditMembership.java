eed565952359fcffa924a42641e13bf5d301e510
Mick3460 <73465505+Mick3460@users.noreply.github.com>
2020-11-25 15:16:24 +0100

    Ændrede menuen i Edit Membership

--- src/com/delphin/EditMembership.java
+++ src/com/delphin/EditMembership.java
@@ -31,16 +31,7 @@ public class EditMembership {
 
     //@author Mick
     //A menu to Display when editing memberships.
-    void menuEditMembership() {
-        System.out.println("\nMenu:");
-        System.out.println("1. Ændre Alder");
-        System.out.println("2. Ændre Navn");
-        System.out.println("3. Ændre Aktivitets-niveau");
-        System.out.println("4. Ændre Konkurrence-status");
-        System.out.println("5. Ændre Aktiv Gæld");
-        System.out.println("6. Vis medlemmets informationer");
-        System.out.println("7. Forlad menuen");
-    }
+
 
     //@author MIck
     //A switch case to edit in a member's data.
@@ -48,22 +39,30 @@ public class EditMembership {
     void editMembership(Member member) throws IOException {
         System.out.println(member.toString());
         boolean stayInLoop = true;
-        while(stayInLoop) {
+        //Menu
 
-            menuEditMembership();
+        String headerText2 = "Menu til at tilføje rekordtider til svømmediscipliner";
+        String leadText2 = "Indtast et tal for at tilføj til specifik disciplin";
+        String[] menuItems2 = {"1. Ændre Alder", "2. Ændre Navn", "3. Ændre Aktivitets-niveau",
+                "4. Ændre Konkurrence-status","5. Ændre Aktiv Gæld","6. Vis medlemmets informationer","7. Forlad menuen"};
+        Menu menu = new Menu(headerText2, leadText2, menuItems2); // Create new menu instance
+
+        //Whileloop to stay in menu
+        while(stayInLoop) {
+            menu.printMenu();
             int inputNumber = UserInput.inputInt(1, 7, "Indtast et tal mellem 1-7");
 
             switch (inputNumber) {
-                case 1:
+                case 1://Set age
                     member.setAge(UserInput.inputInt("Indsæt den nye alder"));
                     Member.calculateJuniorSenior(member);
                     System.out.println("Den nye alder er sat til: "+ member.getAge());
                     break;
-                case 2:
+                case 2://Set name
                     member.setName(UserInput.inputString("Skriv det nye navn: ",true));
                     System.out.println("Det nye navn er sat til: "+ member.getName());
                     break;
-                case 3:
+                case 3://Change activity level (passive/active member)
                     member.setActivity(UserInput.validateStringToBoolean("Aktiv","Passiv",
                             "Er medlemmet aktiv eller passiv?"));
                     if (member.isActivity()==true){
@@ -72,12 +71,12 @@ public class EditMembership {
                         System.out.println("Medlemmet er nu passivt, og kan ikke længere bruge vores svømmehal.");
                     }
                     break;
-                case 4:
+                case 4://Change competitiveness level
                     member.setCompetitor(UserInput.validateStringToBoolean("Kompetitiv","Afslappet",
                             "Er medlemmet kompetitiv eller afslappet?"));
                     System.out.println("Den nye konkurrence tilstand er sat til: "+ member.isCompetitor());
                     break;
-                case 5:
+                case 5://Change arrear-status
                     member.setActiveDebt(UserInput.validateStringToBoolean("Godkendt","Mangelfuld",
                             "Er medlemmets betaling godkendt eller mangelfuld?\nEller tryk 1. for at skylde penge, 2 for at der er betalt"));
                     if (member.isActiveDebt()==true){
@@ -86,10 +85,10 @@ public class EditMembership {
                         System.out.println("Medlemmet skylder ikke længere penge.");
                     }
                     break;
-                case 6:
+                case 6://Print the member-info
                     System.out.println(member.toString());
                     break;
-                case 7:
+                case 7://Leave the loop
                     stayInLoop=false;
                     break;
 
