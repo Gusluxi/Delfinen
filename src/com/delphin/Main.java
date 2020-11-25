020f90a783a060d8b33f7fa24aab6440e44b5296
Frederik André Leonhard <fred05k3@stud.kea.dk>
2020-11-25 12:15:52 +0100

    Start på menuen

--- src/com/delphin/Main.java
+++ src/com/delphin/Main.java
@@ -9,10 +9,11 @@ public class Main {
         EditMembership editMembership = new EditMembership();
         FileEditing fileEditing = new FileEditing();
         IDNumber idNumber = new IDNumber();
+        TimeAndDate timeClass = new TimeAndDate();
 
         //Testcode
-                    //Opretter JESUS som medlem.
-        Member test3 = new Member(69, 35,"Jesus",true,false,true,false,1500);
+        //Opretter JESUS som medlem.
+        Member test3 = new Member(69, 35, "Jesus", true, false, true, false, 1500);
         fileEditing.createNewMemberObjectFile(test3);
 
         //editMembership.newMembership();
@@ -24,31 +25,55 @@ public class Main {
         fileEditing.createNewMemberObjectFile(test); //sætter ham tilbage i fil som object
 
 
-
         System.out.println(fileEditing.findSpecificFileValues("Skriv navn eller #nr. på den person der skal redigeres: ")); //Returner
         fileEditing.dataToArrayList();
 
+        boolean run = true;
+        int choice;
+        String headertext = "Velkommen til Definens system  -  kl. " + timeClass.displayTime();
+        String leadtext = "Indtast en valgmulighed: ";
+        String[] menuitems = {"1. Formandens Menu", "2. Svømmetræner Menu", "3. Kasser Menu", "9. Afslut program."};
+
+        while (run) {
+            Menu menu = new Menu(headertext, leadtext, menuitems);
+            menu.printMenu();
+            choice = UserInput.inputInt(leadtext);
+            switch (choice) {
+                case 1: //Formandens Menu
+                    break;
+                case 2: //Svømmetræner Menu
+                    break;
+                case 3: //Kassér Menu
+                    break;
+                case 9: //Terminates program (if needed).
+                    System.out.println("Afslutter program...");
+                    int confirmEnd = UserInput.inputInt(1, 2, "Er du sikker på at afslutte programmet?\n1. for Ja, 2. for Nej: ");
+                    if (confirmEnd == 1)
+                        run = false;
+                    break;
+                default:
+                    menu.printMenu();
+            }
+        }
     }
 
+            public static void main (String[]args) throws IOException {
+                Main prg = new Main();
+             //   prg.run();
+            }
 
-    public static void main(String[] args) throws IOException {
-        Main prg = new Main();
-        prg.run();
-    }
+            //void gammelKodetest(){
+            //Nedestående tester om vi kan læse en fil, slette en givet string, og så display igen
+            //fileEditing.displayTextFile();
+            //fileEditing.removeLineFromText("hej");
+            //fileEditing.displayTextFile();
 
-    void gammelKodetest(){
-        //Nedestående tester om vi kan læse en fil, slette en givet string, og så display igen
-        //fileEditing.displayTextFile();
-        //fileEditing.removeLineFromText("hej");
-        //fileEditing.displayTextFile();
+            //Nedestående tester vores sortering. IT WORKS LEL
+            //fileEditing.sortTextFile();
 
-        //Nedestående tester vores sortering. IT WORKS LEL
-        //fileEditing.sortTextFile();
+            //fileEditing.readSpecificFile("50");
+            //editMembership.findCrazyMember();
+            //System.out.println(editMembership.findCrazyMember());
 
-        //fileEditing.readSpecificFile("50");
-        //editMembership.findCrazyMember();
-        //System.out.println(editMembership.findCrazyMember());
-
-    }
 }
 
