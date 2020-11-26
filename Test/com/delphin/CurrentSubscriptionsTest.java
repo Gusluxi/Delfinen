288882f9435f4e7a1cb36eab9ab76c663701c898
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2020-11-26 10:54:55 +0100

    JUnit test mappe tilføjet JUnit test lavet af CurrentSubscriptionsTest

---
+++ Test/com/delphin/CurrentSubscriptionsTest.java
@@ -0,0 +1,35 @@
+package com.delphin;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrentSubscriptionsTest {
+
+    //@author Kristian
+    //Test of metheod, adding members subscription prices together through a for-loop.
+    @Test
+    void showTotalRevenue() throws IOException {
+            Member member = new Member();
+
+            ArrayList<Double> subscriptionPrice = new ArrayList<Double>();
+            subscriptionPrice.add(member.SENIORPRICE);
+            subscriptionPrice.add(member.JUNIORPRICE);
+            subscriptionPrice.add(member.JUNIORPRICE);
+            subscriptionPrice.add(member.PASSIVE);
+            subscriptionPrice.add(member.PENSIONER);
+
+            double expected = 5300;
+            double result = 0;
+
+            for (int i = 0; i < subscriptionPrice.size(); i++) {
+                Double tempTotal = subscriptionPrice.get(i);
+                result += tempTotal;
+            }
+            assertEquals(expected, result);
+        }
+
+}
