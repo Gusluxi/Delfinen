35ba33ff0a147159f0f8b0e2dac5cfda09950e03
Mick3460 <73465505+Mick3460@users.noreply.github.com>
2020-11-30 12:23:20 +0100

    Rettet i menuer yderligere.. Jeg tror de er som de skal være nu.

--- src/com/delphin/Main.java
+++ src/com/delphin/Main.java
@@ -1,37 +1,17 @@
 package com.delphin;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
     void run() throws Exception {
         //Instances of classes
+        testSubjects(); // Make a couple of members.
         EditMembership editMembership = new EditMembership();
         FileEditing fileEditing = new FileEditing();
-        IDNumber idNumber = new IDNumber();
-        CurrentSubscriptions currentSubscriptions = new CurrentSubscriptions();
         SwimTimesAndStats swimTimesAndStats = new SwimTimesAndStats();
-        EditUserLogin editUserLogin = new EditUserLogin();
         MenuSwitches menuSwitches = new MenuSwitches();
-
-        //Testcode
-
-        //Opretter medlem.
-        //Member test3 = new Member(1, 35,"Jesus",true,false,true,false,1500);
-        Member stallone = new Member(2, 74,"Sylvester Stallone",true,false,true,false,1500);
-        Member schwarzenegger = new Member(3, 73,"Arnold Schwarzenegger",true,false,true,false,1500);
-        Member jStatham = new Member(4, 53,"Jason Statham",true,false,true,false,1500);
-        Member jClaude = new Member(5, 60,"Jean-Claude Van Damme",true,false,true,false,1500);
-        Member tCrews = new Member(6, 52,"Terry Crews",true,false,true,false,1500);
-        Member jLi = new Member(7, 57,"Jet Li",true,false,true,false,1500);
-
-        //Member test4 = new Member(2, 15,"Alpaca",true,false,true,true,1000);
-        fileEditing.storeInObjectFile(stallone);
-        fileEditing.storeInObjectFile(schwarzenegger);
-        fileEditing.storeInObjectFile(jStatham);
-        fileEditing.storeInObjectFile(jClaude);
-        fileEditing.storeInObjectFile(jLi);
-        fileEditing.storeInObjectFile(tCrews);
 
         //Menu
         boolean run = true;
@@ -125,5 +105,27 @@ public class Main {
         prg.run();
     }
 
+
+
+
+    void testSubjects() throws IOException {
+        FileEditing fileEditing = new FileEditing();
+        //Opretter medlem.
+        //Member test3 = new Member(1, 35,"Jesus",true,false,true,false,1500);
+        Member stallone = new Member(2, 74,"Sylvester Stallone",true,false,true,false,1500);
+        Member schwarzenegger = new Member(3, 73,"Arnold Schwarzenegger",true,false,true,false,1500);
+        Member jStatham = new Member(4, 53,"Jason Statham",true,false,true,false,1500);
+        Member jClaude = new Member(5, 60,"Jean-Claude Van Damme",true,false,true,false,1500);
+        Member tCrews = new Member(6, 52,"Terry Crews",true,false,true,false,1500);
+        Member jLi = new Member(7, 57,"Jet Li",true,false,true,false,1500);
+
+        //Member test4 = new Member(2, 15,"Alpaca",true,false,true,true,1000);
+        fileEditing.storeInObjectFile(stallone);
+        fileEditing.storeInObjectFile(schwarzenegger);
+        fileEditing.storeInObjectFile(jStatham);
+        fileEditing.storeInObjectFile(jClaude);
+        fileEditing.storeInObjectFile(jLi);
+        fileEditing.storeInObjectFile(tCrews);
+    }
 }
 
